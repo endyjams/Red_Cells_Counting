75dc64b1cd0a8aca39ae58a5f4e57a7105f2a415
endyjams <112718430+endyjams@users.noreply.github.com>
2023-04-21 15:50:05 -0300

    Setting Up Environment

---
+++ Red_Cells_Counting/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projeto como parte da avaliação da disciplina Processamento de Imagens ministrada pelo professor Leonardo -> O utilizando técnicas de processamentos de imagens apresentadas em sala, detectar hemácias presentes em uma imagem. A base de dados Kaggle será utilizada para teste e validação desse projeto.',
+    author='Endy',
+    license='MIT',
+)
